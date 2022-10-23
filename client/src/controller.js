4380e1ddbbe71555bcc3b366a3b991b53dc902f7
Nir Nicole <Niko426@gmail.com>
2022-10-23 12:39:09 +0300

    added api call logic and jquery travaersal catches of values

--- client/src/controller.js
+++ client/src/controller.js
@@ -9,33 +9,20 @@ Handlebars.registerHelper("chooseClass", (isPressed) =>
 	isPressed ? "dt-item" : "item"
 )
 
-const generateData = function (attempts = 5, user_input) {
+const generateData = function (attempts, user_input, gluten, dairy) {
 	model
-		.getData(user_input)
+		.getData(user_input, gluten, dairy)
 		.then((res) => renderer.renderResults(res))
 		.catch((error) => errorHandeling(error, attempts, generateData))
 }
 
 $("#submit").on("click", function () {
 	let user_input = $("#user-input").val()
+	let gluten = $("#gluten-active").is(":checked")
+	let dairy = $("#dairy-active").is(":checked")
 	if (user_input != "") {
 		model.initData()
-		generateData(5, user_input)
-	} else console.warn("no input")
-})
-
-$("#post").on("click", function () {
-	let num_input = $("#user-post").val()
-	let val_input = $("#user-post-val").val()
-	let dataObject = {}
-	if (num_input != "" && val_input != "") {
-		dataObject[num_input] = val_input
-		console.log(dataObject)
-		model.initData()
-		model
-			.addData(dataObject)
-			.then((res) => renderer.renderResults(res))
-			.catch((error) => errorHandeling(error, attempts, generateData))
+		generateData(5, user_input, gluten, dairy)
 	} else console.warn("no input")
 })
 
@@ -45,22 +32,22 @@ $("body").bind("keypress", function (event) {
 	}
 })
 
-$("#results").on("click", ".dreamteam-card-button", function () {
-	let isDreamTeam = $(this).parent("div").attr("data-dt")
-	let player_id = $(this).parent("div").attr("data-id")
-	let player_data = model.getCache().find((p) => p.id == player_id)
-	model.initDreamTeam()
+// $("#results").on("click", ".dreamteam-card-button", function () {
+// 	let isDreamTeam = $(this).parent("div").attr("data-dt")
+// 	let player_id = $(this).parent("div").attr("data-id")
+// 	let player_data = model.getCache().find((p) => p.id == player_id)
+// 	model.initDreamTeam()
 
-	if (String(isDreamTeam).toLowerCase() == "true") {
-		model
-			.deletePlayer(player_id)
-			.then((res) => renderer.renderCard(player_id, res.metaData))
-	} else {
-		model
-			.addPlayer(player_data)
-			.then((res) => renderer.renderCard(player_id, res.metaData))
-	}
-})
+// 	if (String(isDreamTeam).toLowerCase() == "true") {
+// 		model
+// 			.deletePlayer(player_id)
+// 			.then((res) => renderer.renderCard(player_id, res.metaData))
+// 	} else {
+// 		model
+// 			.addPlayer(player_data)
+// 			.then((res) => renderer.renderCard(player_id, res.metaData))
+// 	}
+// })
 
 let errorHandeling = function (error, attempts, callback) {
 	console.warn(error)
